8bf06830f99d30b4da3e981fff97faf7c3e88398
Wanderingtech <heather86lyn@gmail.com>
2020-08-27 19:29:50 -0700

    API routes

---
+++ routes/api.js
@@ -0,0 +1,32 @@
+var router = require("express").Router()
+var db = require("../models")
+
+router.get("/api/workouts",function(req,res){
+    db.find()
+    .then(function(exercises){
+        console.log("Get route",exercises)
+        res.json(exercises)
+    })
+})
+router.put("/api/workouts/:id",function(req,res){
+    db.findByIdAndUpdate(req.params.id,
+        {$push:{exercises: req.body}},{new: true})
+    .then(function(exercises){
+        console.log("Put route",exercises)
+        res.json(exercises)
+    })
+})
+router.post("/api/workouts",function(req,res){
+    db.create(req.body)
+    .then(function(exercises){
+        console.log("Post route",exercises)
+        res.json(exercises)
+    })
+})
+router.get("/api/workouts/range",function(req,res){
+    db.find({}).limit(7)
+    .then(function(exercises){
+        console.log("Get range route",exercises)
+        res.json(exercises)
+    })
+})
